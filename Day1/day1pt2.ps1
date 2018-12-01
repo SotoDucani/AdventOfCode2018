930eb8812701eb913ed67146d1b14a6c8751e06a
Joshua Herrmann <jdvh1292@gmail.com>
2018-12-01 15:20:29 -0500

    Functionize and include execution time

--- Day1/day1pt2.ps1
+++ Day1/day1pt2.ps1
@@ -1,23 +1,25 @@
-$input = Get-Content -Path .\Day1\input.txt
+function Day1Pt2 {
+    $input = Get-Content -Path .\Day1\input.txt
 
-$SeenFreq = New-Object System.Collections.Generic.List[System.Object]
-$sum=0
-$TwiceSeen=$false
-$CurIteration = 0
-while ($TwiceSeen -eq $false) {
-    $CurIteration += 1
-    Write-Host "Iteration: $($CurIteration)"
-    foreach ($num in $input) {
-        #Write-Host "Adding $($num) to $($sum)"
-        $sum = $sum + [int]$num
-        #Write-Host "CurFreq: $($Sum)"
-        if ($SeenFreq -notcontains $sum) {
-            $SeenFreq.add($sum)
-            #Write-Host $SeenFreq
-        } else {
-            Write-Host "Twice Seen: $($sum)"
-            $TwiceSeen = $true
-            break
+    $SeenFreq = New-Object System.Collections.Generic.List[System.Object]
+    $sum=0
+    $TwiceSeen=$false
+    $CurIteration = 0
+    while ($TwiceSeen -eq $false) {
+        $CurIteration += 1
+        Write-Host "Iteration: $($CurIteration)"
+        foreach ($num in $input) {
+            #Write-Host "Adding $($num) to $($sum)"
+            $sum = $sum + [int]$num
+            #Write-Host "CurFreq: $($Sum)"
+            if ($SeenFreq -notcontains $sum) {
+                $SeenFreq.add($sum)
+                #Write-Host $SeenFreq
+            } else {
+                Write-Host "Twice Seen: $($sum)"
+                $TwiceSeen = $true
+                break
+            }
         }
     }
 }
