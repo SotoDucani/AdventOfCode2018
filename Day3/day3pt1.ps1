cf43e7219b7dd78f7b86daa35b959d85f379e5c2
Joshua Herrmann <jdvh1292@gmail.com>
2018-12-12 13:32:53 -0500

    Switch to Hashtables; It Works!
    Hashtables have better lookup speed. Biggest time sink is the actual creation of the nested hashtables

--- Day3/day3pt1.ps1
+++ Day3/day3pt1.ps1
@@ -1,47 +1,60 @@
 function day3pt1 {
-    #$day3input = Get-Content ".\Day3\input.txt"
-    $day3input = Get-Content ".\Day3\exampleinput.txt"
+    $SizeX = 1000
+    $SizeY = 1000
 
-    #$fabricX = 1000
-    #$fabricY = 1000
-    $fabricX = 8
-    $fabricY = 8
+    $fabricSheet = New-Object System.Collections.Hashtable
 
-    $fabricSheet = New-Object System.Collections.ArrayList
-
-    Write-Host "Fabric X: $($fabricX)"
-    Write-Host "Fabric Y: $($fabricY)"
-
-    $curY = 0
-    while ($curY -lt $fabricY) {
-        $curX = 0
-        while ($curX -lt $fabricX) {
-            Write-Host "Building at: $($curX),$($curY)"
-            $fabricSheet.Add(@{locX = $curX; locY = $curY; claims = @()}) | Out-Null
-            $curX += 1
+    $curX = 0
+    while ($curX -lt $SizeX) {
+        Write-Host "Building X: $($curX)"
+        $curY = 0
+        $fabricSheet.add($curX, [System.Collections.Hashtable]@{}) | Out-Null
+        while ($curY -lt $SizeY) {
+            #Write-Host "Building Y: $($curY)"
+            $fabricSheet[$curX].add($curY,[System.Collections.ArrayList]@()) | Out-Null
+            $curY += 1
         }
-        $curY += 1
+        $curX += 1
     }
-    Write-Host "Fabric Squares: $($fabricSheet.Count)"
+
+    $day3input = Get-Content ".\Day3\input.txt"
 
     $claimList = New-Object System.Collections.ArrayList
     foreach ($line in $day3input) {
         $split = $line.split("#").split("@").Split(",").Split(":").split("x").Trim()
         $claimList.Add(@{id = $split[1]; topLeftX = $split[2]; topLeftY = $split[3]; sizeX = $split[4]; sizeY = $split[5]}) | Out-Null
     }
-    Write-Host "Claims: $($claimList.Count)"
 
+    Write-Host "Marking claims on Fabric Sheet"
     foreach ($claim in $claimList) {
-        $markerY = $claim.topLeftY
-        while ($markerY -lt ($claim.topLeftY + $claim.sizeY)) {
-            $markerX = $claim.topLeftX
-            while ($markerX -lt ($claim.topLeftX + $claim.sizeX)) {
-                
-                $markerX += 1
+        $markerX = [int]$claim.topLeftX
+        while ($markerX -lt $([int]$claim.topLeftX + [int]$claim.sizeX)) {
+            #Write-Host "Marking Column: $($markerX)"
+            $markerY = [int]$claim.topLeftY
+            while ($markerY -lt $([int]$claim.topLeftY + [int]$claim.sizeY)) {
+                #Write-Host "Marking Row: $($markerY)"
+                $fabricSheet[$markerX][$markerY].Add($Claim.ID) | Out-Null
+                $markerY += 1
             }
-            $markerY += 1
+            $markerX += 1
         }
     }
 
-    return $fabricSheet,$claimList
+    $sizeX = $fabricSheet.Count
+    $sizeY = $fabricSheet[0].count
+
+    $claimsOverTwo = 0
+
+    $posX = 0
+    while ($posX -lt $sizeX) {
+        $posY = 0
+        while ($posY -lt $sizeY) {
+            if ($fabricSheet[$posX][$posY].Count -ge 2) {
+                $claimsOverTwo += 1
+            }
+            $posY += 1
+        }
+        $posX += 1
+    }
+    return $claimsOverTwo
 }
